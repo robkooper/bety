d06ce99c24135e37f6d79310b5d6ad7a19f39713
David LeBauer <dlebauer@gmail.com>
2014-04-01 22:06:43 -0500

    updated gh-pages using pandoc markdown_github

--- wikimd2html.sh
+++ wikimd2html.sh
@@ -1,2 +1,4 @@
 for file in *.md; do sudo grip --gfm --context=PecanProject/bety --export  title.md; done
 for file in *.md; do python -m markdown title.md > title.md.html; done; 
+for file in *.md; do ~/.cabal/bin/pandoc --from markdown_github --to html --standalone title.md -o title.md.html; done
+rename 's/\.md//' *.md.html
